e8a1279fe8d127cb0f9115979df1864b18ebf18b
Dean Foster <Deanfoster11@gmail.com>
2021-10-08 21:13:24 -0400

    Remove express routers

--- express/app.js
+++ express/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const janusURL = 'localhost:8088/janus';
 
 var app = express();
 
@@ -14,7 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// API routes
+app.post('/', async function(req, res) {
+    
+});
 
 module.exports = app;
