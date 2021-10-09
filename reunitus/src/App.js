13dc7464a150f8b5b6f6b8808e7bcfc5b50349e3
Dean Foster <Deanfoster11@gmail.com>
2021-10-08 21:51:26 -0400

    Do some stuff

--- reunitus/src/App.js
+++ reunitus/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Room from './components/Room';
 import Home from './components/Home';
+import CreateRoom from './components/CreateRoom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -18,6 +19,7 @@ class App extends React.Component {
               <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/room" component={Room} />
+                <Route path="/create-room" component={CreateRoom} />
               </Switch>
             </React.Fragment>
           </div>
--- reunitus/src/components/Home.js
+++ reunitus/src/components/Home.js
@@ -2,34 +2,28 @@ import logo from "../images/logo.svg";
 import React from 'react';
 
 class Home extends React.Component {
-		constructor(props) {
-				super(props);
-				this.handleJoinRoom = this.handleJoinRoom.bind(this);
-		}
-		componentDidMount() {
-		}
+	constructor(props) {
+		super(props);
+	}
 
-		handleJoinRoom() {
-				this.props.history.push('/room');
-		}
-
-		render() {
-				return (
-						<div className="App">
-								<header className="App-header">
-										<img src={logo} className="App-logo" alt="logo" />
-										<p>
-												Welcome to <code>Reunitus</code> video room (powered by Janus).
-										</p>
-										<button
-												onClick={this.handleJoinRoom}
-										>
-												Join Room
-										</button>
-								</header>
-						</div>
-				)
-		}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Welcome to Janus test app.
+					</p>
+					<button onClick={() => this.props.history.push('/room')}>
+						Create new room
+					</button>
+					<button onClick={() => this.props.history.push('/create-room')}>
+						Join Room
+					</button>
+				</header>
+			</div>
+		)
+	}
 }
 
 export default Home;
