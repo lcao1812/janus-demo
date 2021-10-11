22240915e7b6a9b8761d2c0abf4f4f7d5d45ace4
ldc1812 <locdcao@gmail.com>
2021-10-11 12:26:08 -0400

    Added destroy and create room routes

--- express/app.js
+++ express/app.js
@@ -17,63 +17,178 @@ app.use(cookieParser());
 app.use(cors());
 
 // API routes
-app.post('/create-room/:room', async function(req, res) {
-    // Create new room
-    const roomid = req.params['room'];
-    
+app.post('/create-room/:roomID', async function (req, res, next) {
+	// Create new room
+	let { roomID } = req.params;
+	roomID = parseInt(roomID);
+	let endpoint = janusURL;
+	try {
+		let result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'Create',
+				transaction: Janus.randomString()
+			})
+		});
+		let body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const sessionId = body['data']['id'];
+		endpoint += '/' + sessionId; // http://localhost:8088/janus/<sessionid>
+
+		// Attach session to videoroom plugin
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'attach',
+				plugin: 'janus.plugin.videoroom',
+				transaction: Janus.randomString()
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const pluginId = body['data']['id'];
+		endpoint += '/' + pluginId; // http://localhost:8088/janus/<session>/<pluginid>
+
+		// Fetch a create request for the session
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'message',
+				transaction: Janus.randomString(),
+				body: {
+					request: 'create',
+					room: roomID
+				}
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const room = body['plugindata']['data'];
+
+		// send response
+		res.json(room);
+	} catch (error) {
+		console.log(err);
+		next();
+	}
 });
 
-app.get('/list-rooms', async function(req, res, next) {
-    try {
-        let endpoint = janusURL;
-        // Find existing rooms; create new session
-        let result = await fetch(endpoint, {
-            method: 'POST',
-            body: JSON.stringify({
-                "janus": "Create",
-                "transaction": Janus.randomString()
-            })
-        });
-        let body = await result.json();
-        if (body["janus"] != "success") return next(body);
-        const sessionId = body["data"]["id"];
-        endpoint += '/' + sessionId; // http://localhost:8088/janus/<sessionid>
+app.post('/destroy-room/:roomID', async function (req, res, next) {
+	// Create new room
+	let { roomID } = req.params;
+	roomID = parseInt(roomID);
+	let endpoint = janusURL;
+	try {
+		let result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'Create',
+				transaction: Janus.randomString()
+			})
+		});
+		let body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const sessionId = body['data']['id'];
+		endpoint += '/' + sessionId; // http://localhost:8088/janus/<sessionid>
 
-        // Attach session to videoroom plugin
-        result = await fetch(endpoint, {
-            method: 'POST', 
-            body: JSON.stringify({
-                "janus": "attach", 
-                "plugin": "janus.plugin.videoroom",
-                "transaction": Janus.randomString()
-            })
-        });
-        body = await result.json();
-        if (body["janus"] != 'success') return next(body);
-        const pluginId = body["data"]["id"];
-        endpoint += '/' + pluginId; // http://localhost:8088/janus/<session>/<pluginid>
+		// Attach session to videoroom plugin
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'attach',
+				plugin: 'janus.plugin.videoroom',
+				transaction: Janus.randomString()
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const pluginId = body['data']['id'];
+		endpoint += '/' + pluginId; // http://localhost:8088/janus/<session>/<pluginid>
 
-        // List created rooms; talk to video room plugin
-        result = await fetch(endpoint, {
-            method: "POST", 
-            body: JSON.stringify({
-                "janus": "message", 
-                "transaction": Janus.randomString(),
-                body: {
-                    "request": "list"
-                }
-            })
-        });
-        body = await result.json();
-        if (body["janus"] != 'success') return next(body);
-        const rooms = body["plugindata"]["data"]["list"];
-        
-        // Send response
-        res.json(rooms);
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
+		// Fetch a create request for the session
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'message',
+				transaction: Janus.randomString(),
+				body: {
+					request: 'destroy',
+					room: roomID
+				}
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const room = body['plugindata']['data'];
+
+		// send response
+		res.json(room);
+	} catch (error) {
+		console.log(err);
+		next();
+	}
+});
+
+app.get('/list-rooms', async function (req, res, next) {
+	try {
+		let endpoint = janusURL;
+		// Find existing rooms; create new session
+		let result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'Create',
+				transaction: Janus.randomString()
+			})
+		});
+		let body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const sessionId = body['data']['id'];
+		endpoint += '/' + sessionId; // http://localhost:8088/janus/<sessionid>
+
+		// Attach session to videoroom plugin
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'attach',
+				plugin: 'janus.plugin.videoroom',
+				transaction: Janus.randomString()
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const pluginId = body['data']['id'];
+		endpoint += '/' + pluginId; // http://localhost:8088/janus/<session>/<pluginid>
+
+		// List created rooms; talk to video room plugin
+		result = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				janus: 'message',
+				transaction: Janus.randomString(),
+				body: {
+					request: 'list'
+				}
+			})
+		});
+		body = await result.json();
+		if (body['janus'] != 'success') return next(body);
+		const rooms = body['plugindata']['data']['list'];
+
+		// Send response
+		res.json(rooms);
+	} catch (err) {
+		console.error(err);
+		next(err);
+	}
 });
 
 export default app;
+// //Start server
+// var port = 5000;
+// app.listen(port, function () {
+// 	console.log(
+// 		'Express server listening on port %d in %s mode',
+// 		port,
+// 		app.settings.env
+// 	);
+// });
--- reunitus/src/App.js
+++ reunitus/src/App.js
@@ -3,46 +3,66 @@ import React from 'react';
 import './App.css';
 import Room from './components/Room';
 import Home from './components/Home';
-import CreateRoom from './components/CreateRoom'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CreateRoom from './components/CreateRoom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {username: '', roomid: '', roomsList: []};
-  }
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = { username: '', roomid: '', roomsList: [] };
+	}
 
-  async componentDidMount() {
+	async componentDidMount() {
 		try {
 			let result = await fetch('http://localhost:3001/list-rooms', {
-			  method: 'GET'
+				method: 'GET'
 			});
 			let body = await result.json();
-      this.setState({roomsList: body});
+			this.setState({ roomsList: body });
 		} catch (err) {
 			console.error(err);
-      alert('Could not fetch rooms list');
+			alert('Could not fetch rooms list');
 		}
 	}
 
-  handleSubmit(username, roomid) {
-    this.setState({username, roomid});
-  }
+	handleSubmit(username, roomid) {
+		this.setState({ username, roomid });
+	}
 
-  render() {
-    return (
-        <Router>
-          <div className="main-container">
-            <React.Fragment>
-              <Switch>
-                <Route path="/" exact render={(p) => <Home {...p} onSubmit={this.handleSubmit} rooms={this.state.roomsList} />} />
-                <Route path="/room" render={(p) => <Room {...p} username={this.state.username} roomid={this.state.roomid} />} />
-              </Switch>
-            </React.Fragment>
-          </div>
-        </Router>)
-  }
+	render() {
+		return (
+			<Router>
+				<div className="main-container">
+					<React.Fragment>
+						<Switch>
+							<Route
+								path="/"
+								exact
+								render={(p) => (
+									<Home
+										{...p}
+										onSubmit={this.handleSubmit}
+										rooms={this.state.roomsList}
+									/>
+								)}
+							/>
+							<Route
+								path="/room"
+								render={(p) => (
+									<Room
+										{...p}
+										username={this.state.username}
+										roomid={this.state.roomid}
+									/>
+								)}
+							/>
+						</Switch>
+					</React.Fragment>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
